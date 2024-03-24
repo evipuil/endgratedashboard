093bfe8254ef3766968157b33ff603e8fce1fc6d
evipuil <126958435+evipuil@users.noreply.github.com>
2024-03-24 13:44:08 -0400

    Add files via upload
    Finished front-end

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
+# Libraries
 from flask import Flask, render_template, request, url_for, flash, redirect, send_from_directory, jsonify
 import json
+import numpy as np
 import plotly
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import atexit
 import requests
 from datetime import datetime
-from keys import ENDGRATE_API_KEY
+from keys import ENDGRATE_API_KEY, SECRET_KEY, APPLICATION_URL
 
+# Defines Web Application
 app = Flask(__name__) # defines web application
-app.config['SECRET_KEY'] = 'acad04e0262e72720856b95f9b9e74a2990314e0009e8d36' # creates secret key
+app.config['SECRET_KEY'] = SECRET_KEY # creates secret key
 
-APPLICATION_URL = "https://6m3g4p-5000.csb.app/"
-
+# Configuring Payload & Headers
 payload = {
     "configuration_webhook": { "endpoint": f"{APPLICATION_URL}callback" },
     "provider": "googleanalytics",
     "schema": [{ "endgrate_type": "analytics-page_analytics" }]
 }
-
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
     "authorization": f"Bearer {ENDGRATE_API_KEY}"
 }
 
+# Get Session Id With Post Request
 response = requests.post("https://endgrate.com/api/pull/initiate", json=payload, headers=headers)
-
 ENDGRATE_SESSION_ID = response.json()["session_id"]
 print(f"Endgrate Session ID: {ENDGRATE_SESSION_ID}")
 
+# Defining Auth Variables
 AUTHENTICATED = False
 LAST_FETCH = None
 OLD_DATA = None
 
-
+# Fetch Endgrate Data
 def get_endgrate_data():
+    # Global Variables for Recording Data
     global LAST_FETCH
     global OLD_DATA
     if LAST_FETCH != None:
@@ -43,6 +47,7 @@ def get_endgrate_data():
             return OLD_DATA
     LAST_FETCH = datetime.now()
 
+    # Payload & Headers
     payload = {
         "session_id": ENDGRATE_SESSION_ID,
         "endgrate_type": "analytics-page_analytics",
@@ -54,60 +59,73 @@ def get_endgrate_data():
         "authorization": f"Bearer {ENDGRATE_API_KEY}"
     }
 
+    # Transfer Id
     response = requests.post("https://endgrate.com/api/pull/transfer", json=payload, headers=headers)
-
-    
     transfer_id = response.json()["transfer_id"]
 
+    # Get Data
     url = f"https://endgrate.com/api/pull/data?endgrate_type=analytics-page_analytics&transfer_id={transfer_id}"
-
     response = requests.get(url, headers={"accept": "application/json","authorization": f"Bearer {ENDGRATE_API_KEY}"})
-
     data = response.json()
 
-    pathes = {}
+    # Save & Return Data
+    OLD_DATA = data
+    return data
 
-
-
-    OLD_DATA = response.json()
-
-    return response.json()
-
+# Ending Session
 def delete_session():
-    import requests
-
     payload = { "session_id": f"{ENDGRATE_SESSION_ID}" }
-
     requests.post("https://endgrate.com/api/session/delete", json=payload, headers=headers)
 
-@app.route("/get_data")
-def get_data():
-    data = get_endgrate_data()
-    return jsonify(data)
-    
-    
-@app.route('/callback', methods=["POST"])
-def endgrate_callback():
-    global AUTHENTICATED
-    AUTHENTICATED = True
-    return "OK"
-
+# Authenticating With Endgrate
 @app.route('/auth')
 def auth():
+    global AUTHENTICATED
+    AUTHENTICATED = True
     return redirect("https://endgrate.com/session?session_id=" + ENDGRATE_SESSION_ID)
 
-@app.route('/') # home page
+# Home Page
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/google_analytics') # google analytics
+# Google Analytics
+@app.route('/google_analytics')
 def google_analytics():
-    if AUTHENTICATED:
-        return render_template('google_analytics.html', do_auth=False) #return output page
-    
-    return render_template('google_analytics.html', do_auth=True) #return output page
+    if AUTHENTICATED: 
+        data=get_endgrate_data()
+        for a in range(5):
+            if data == "<Response 540 bytes [200 OK]>":
+                data=get_endgrate_data()
+        datapoints=data["transfer_data"]
+        dictionary={}
 
+        for b in range(len(datapoints)):
+            items=datapoints[b]["data"]
+            keys=list(items.keys())
+            for c in range(len(items)):
+                if type(items[keys[c]]) == float or type(items[keys[c]]) == int:
+                    if b == 0:
+                        dictionary[keys[c]]=[items[keys[c]]]
+                    else:
+                        dictionary[keys[c]].append(items[keys[c]])
+
+        #Grid of Subplots for Information
+        fig=make_subplots(rows=(len(dictionary)//2), cols=2)
+        for d in range(len(dictionary)):
+            fig.add_trace(go.Scatter(y=np.array(dictionary[list(dictionary.keys())[d]]), name=list(dictionary.keys())[d]), row=((d//2)+1), col=((d%2)+1))
+        
+        # Export Plot as JSON
+        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # Return Output Page
+        return render_template('google_analytics.html', plot=graphJSON)
+    
+    return render_template('authentication.html', do_auth=True) #return output page
+
+# Running Main
 if __name__ == '__main__':
     app.run(debug=True) # run application
 
-atexit.register(delete_session) # delete session on exit
+# Delete Session on Exit
+atexit.register(delete_session)
